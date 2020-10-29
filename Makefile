df756838ea586cbc85dca93f66c0cc4349a21828
Agrim Sharma <agrimsharma@iisc.ac.in>
2020-10-29 19:45:43 +0530

    First commit

---
+++ Makefile
@@ -0,0 +1,12 @@
+# Makefile for hw0 of DSA and BigData
+# Created by Agrim Sharma (SR no 15275)
+
+CC=g++
+
+all: clean main.o
+
+main.o:
+    $(CC)  main.cpp -o main.o
+
+clean:
+	-rm -r main.o
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout<<"Hello world\n";
+    }
+    
+    return 0;
+}
